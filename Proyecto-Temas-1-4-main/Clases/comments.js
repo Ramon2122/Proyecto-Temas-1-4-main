9bd4a67866389c79b5487537a0ad5701eb28ccb7
Ramon2122 <monjr2002@gmail.com>
2024-12-04 13:46:50 +0100

    first commit

---
+++ Proyecto-Temas-1-4-main/Clases/comments.js
@@ -0,0 +1,9 @@
+export class Comments {
+    constructor(postId,id, name, email, body) {
+        this.postId = postId;
+        this.id = id;
+        this.name = name;
+        this.email = email;
+        this.body = body;
+    }
+}
